fab1e2f37e48cf13e88098e495cec6bf2a281ec8
corgifist <86985149+corgifist@users.noreply.github.com>
2021-12-01 20:30:43 +0300

    Delete utils directory

--- utils/Function.java
+++
@@ -1,9 +0,0 @@
-package com.barley.utils;
-
-import com.barley.runtime.BarleyValue;
-
-import java.io.Serializable;
-
-public interface Function extends Serializable {
-    BarleyValue execute(BarleyValue... args);
-}
